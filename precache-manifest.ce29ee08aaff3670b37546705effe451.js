b00999984f1809af9a4e181caedf2a65f05ca335
charanroot <charan.sjd@gmail.com>
2020-02-17 22:35:53 -0600

    Updates

---
+++ precache-manifest.ce29ee08aaff3670b37546705effe451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f67f7f4ff7d25f4eab4248cf249b73",
+    "url": "/travtail-pwa/index.html"
+  },
+  {
+    "revision": "ecbbc8306e2792b311e5",
+    "url": "/travtail-pwa/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b9bffcaa6cce9e9ffb0f",
+    "url": "/travtail-pwa/static/js/2.cdb7485f.chunk.js"
+  },
+  {
+    "revision": "98f8cfd2928fc840f744fbee57b605eb",
+    "url": "/travtail-pwa/static/js/2.cdb7485f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbbc8306e2792b311e5",
+    "url": "/travtail-pwa/static/js/main.53863895.chunk.js"
+  },
+  {
+    "revision": "b59c074eb254551cdf75",
+    "url": "/travtail-pwa/static/js/runtime-main.7273cfbd.js"
+  },
+  {
+    "revision": "71e1123928c80da1823565e0a2907b90",
+    "url": "/travtail-pwa/static/media/background.71e11239.jpeg"
+  }
+]);
