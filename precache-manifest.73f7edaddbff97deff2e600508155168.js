47c61a4a04ea4b36ff711800366b79d78c1e0ca5
= <charan.sjd@gmail.com>
2020-02-19 16:00:51 -0500

    Updates

---
+++ precache-manifest.73f7edaddbff97deff2e600508155168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f64495d13d22036cffe43dc86d452aa",
+    "url": "/travtail-pwa/index.html"
+  },
+  {
+    "revision": "b4236a0cce1c17ef9fa9",
+    "url": "/travtail-pwa/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d89fe8ed563af083007d",
+    "url": "/travtail-pwa/static/js/2.26750957.chunk.js"
+  },
+  {
+    "revision": "98f8cfd2928fc840f744fbee57b605eb",
+    "url": "/travtail-pwa/static/js/2.26750957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4236a0cce1c17ef9fa9",
+    "url": "/travtail-pwa/static/js/main.29849fdd.chunk.js"
+  },
+  {
+    "revision": "2a6fc6ff0a286303c2aa",
+    "url": "/travtail-pwa/static/js/runtime-main.37ffb553.js"
+  },
+  {
+    "revision": "71e1123928c80da1823565e0a2907b90",
+    "url": "/travtail-pwa/static/media/background.71e11239.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b16373b32e2ec308535b81ee4a891ea.js"
+  "/travtail-pwa/precache-manifest.73f7edaddbff97deff2e600508155168.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/travtail-pwa/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
